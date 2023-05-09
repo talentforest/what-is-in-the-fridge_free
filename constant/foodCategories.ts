4f5a68eca87843d888ea895164caa106eb9544e7
jellie <talentforest0501@gmail.com>
2023-05-09 21:01:05 +0900

    update food category

--- constant/foodCategories.ts
+++ constant/foodCategories.ts
@@ -4,26 +4,30 @@ interface FoodCategory {
 }
 
 export type Category =
-  | '채소, 과일'
+  | '채소'
+  | '과일'
   | '우유, 계란, 유제품'
   | '정육, 수산'
-  | '밀키트'
-  | '양념, 장류, 오일'
+  | '밀키트, 간편식'
+  | '양념, 조미료, 오일'
   | '아이스크림, 과자, 초콜릿'
-  | '견과류, 쌀, 면'
+  | '곡류, 면'
+  | '견과류, 콩류'
   | '빵, 떡, 잼'
   | '통조림, 햄'
   | '생수, 음료수, 커피';
 
 export const foodCategories: FoodCategory[] = [
-  { id: 1, category: '채소, 과일' },
-  { id: 2, category: '우유, 계란, 유제품' },
-  { id: 3, category: '정육, 수산' },
-  { id: 4, category: '밀키트' },
-  { id: 5, category: '양념, 장류, 오일' },
-  { id: 6, category: '아이스크림, 과자, 초콜릿' },
-  { id: 7, category: '견과류, 쌀, 면' },
-  { id: 8, category: '빵, 떡, 잼' },
-  { id: 9, category: '통조림, 햄' },
-  { id: 10, category: '생수, 음료수, 커피' },
+  { id: 1, category: '채소' },
+  { id: 2, category: '과일' },
+  { id: 3, category: '우유, 계란, 유제품' },
+  { id: 4, category: '정육, 수산' },
+  { id: 5, category: '밀키트, 간편식' },
+  { id: 6, category: '양념, 조미료, 오일' },
+  { id: 7, category: '아이스크림, 과자, 초콜릿' },
+  { id: 8, category: '곡류, 면' },
+  { id: 9, category: '견과류, 콩류' },
+  { id: 10, category: '빵, 떡, 잼' },
+  { id: 11, category: '통조림, 햄' },
+  { id: 12, category: '생수, 음료수, 커피' },
 ];
