d43d41f9c1ce978c44345a958aaaca75574c25ea
jellie <talentforest0501@gmail.com>
2023-05-11 01:37:53 +0900

    refactoring: useAddFood hook, useCheckFood hook

--- hooks/useAddFood.tsx
+++ hooks/useAddFood.tsx
@@ -4,30 +4,30 @@ import { Food, FoodInfo, initialFoodInfo } from '../constant/foods';
 import { useState } from 'react';
 import { CompartmentNum, Space } from '../constant/fridge';
 import { addFood } from '../redux/slice/allFoodsSlice';
+import { Alert } from 'react-native';
+import { getISODate } from '../util';
 import UUIDGenerator from 'react-native-uuid';
+import useCheckFood from './useCheckFood';
 
 interface Props {
-  food?: Food;
-  space: Space;
-  compartmentNum: CompartmentNum;
+  space?: Space;
+  compartmentNum?: CompartmentNum;
 }
 
-export default function useAddFood({ food, space, compartmentNum }: Props) {
+export default function useAddFood({ space, compartmentNum }: Props) {
+  const { checkExistFood } = useCheckFood();
   const myUuid = UUIDGenerator.v4();
   const dispatch = useDispatch();
 
-  const foodInfo = food ? food : initialFoodInfo;
-  const [newFood, setNewFood] = useState<Food>({
-    ...foodInfo,
-    space,
-    compartmentNum,
-  });
+  // 새로운 식료품 추가
+  const initialFood = { ...initialFoodInfo, space, compartmentNum };
+  const [newFood, setNewFood] = useState(initialFood as Food);
 
   const changeFoodInfo = (newInfo: FoodInfo) => {
     return setNewFood({ ...newFood, ...newInfo });
   };
 
-  const onSubmit = () => {
+  const onSubmitFromForm = () => {
     const newFoodInfo: Food = { ...newFood, id: myUuid as string };
     if (newFoodInfo.favorite) {
       dispatch(addFavorite(newFoodInfo));
@@ -35,9 +35,31 @@ export default function useAddFood({ food, space, compartmentNum }: Props) {
     dispatch(addFood(newFoodInfo));
   };
 
+  // FoodSpaceModal에서 식료품 추가
+  const onSubmitFromSpaceModal = (space: Space, food: Food) => {
+    if (checkExistFood(food)) {
+      return Alert.alert(
+        `${food.name}`,
+        `${food.space} ${food.compartmentNum}에 이미 식료품이 있습니다.`
+      );
+    }
+
+    const favoriteFoodInfo: Food = {
+      ...food,
+      expirationDate: getISODate(new Date()),
+      purchaseDate: getISODate(new Date()),
+      space,
+      compartmentNum: '1번',
+      id: myUuid as string,
+    };
+    dispatch(addFood(favoriteFoodInfo));
+    Alert.alert(`${food.name} 추가`, `${space} 1번에 추가되었습니다.`);
+  };
+
   return {
     newFood,
     changeFoodInfo,
-    onSubmit,
+    onSubmitFromForm,
+    onSubmitFromSpaceModal,
   };
 }
