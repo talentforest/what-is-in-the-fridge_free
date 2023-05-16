bd6f7ee106352176b02de1a8ae8f87aaf1fbcf3b
jellie <talentforest0501@gmail.com>
2023-05-16 18:09:46 +0900

    feat: redux-persist & AsyncStorage

--- redux/store.ts
+++ redux/store.ts
@@ -1,15 +1,50 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import {
+  persistStore,
+  persistReducer,
+  PersistConfig,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import fridgeFoodsReducer from './slice/allFoodsSlice';
 import favoriteFoodsReducer from './slice/favoriteFoodsSlice';
 import shoppingListReducer from './slice/shoppingList';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const reducers = {
+  allFoods: fridgeFoodsReducer,
+  favoriteFoods: favoriteFoodsReducer,
+  shoppingList: shoppingListReducer,
+};
+
+const rootReducer = combineReducers({ ...reducers });
+
+type ReducersState = ReturnType<typeof rootReducer>;
+
+const persistConfig: PersistConfig<ReducersState> = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    allFoods: fridgeFoodsReducer,
-    favoriteFoods: favoriteFoodsReducer,
-    shoppingList: shoppingListReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
