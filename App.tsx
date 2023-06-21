ed4907bb3ca21e60cb575365b79d3ee6957b13d6
jellie <talentforest0501@gmail.com>
2023-06-22 01:08:01 +0900

    delete: react-query

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider as ReduxProvider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import Navigation from './navigation/Navigation';
 import Splash from './screens/Splash';
 import 'react-native-gesture-handler';
@@ -24,20 +23,16 @@ export default function App() {
     );
   }
 
-  const client = new QueryClient();
-
   return (
     <ReduxProvider store={store}>
-      <QueryClientProvider client={client}>
-        <PersistGate loading={null} persistor={persistor}>
-          <SafeAreaProvider>
-            <NavigationContainer>
-              <Navigation />
-              <StatusBar style='auto' />
-            </NavigationContainer>
-          </SafeAreaProvider>
-        </PersistGate>
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <Navigation />
+            <StatusBar style='auto' />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </PersistGate>
     </ReduxProvider>
   );
 }
