3534cbc776bce55ccc59de285eff95e9c3b816f9
jellie <talentforest0501@gmail.com>
2023-07-27 17:01:51 +0900

    bug fix: check foods in last compartment in CountBtn

--- components/common/Buttons/CountBtn.tsx
+++ components/common/Buttons/CountBtn.tsx
@@ -1,4 +1,4 @@
-import { BLUE, ORANGE_RED } from '../../../constant/colors';
+import { DEEP_BLUE, LIGHT_GRAY, ORANGE_RED } from '../../../constant/colors';
 import { scaleH } from '../../../util';
 import { TouchableOpacity } from '../../native-component';
 import Icon from '../../native-component/Icon';
@@ -7,22 +7,24 @@ import tw from 'twrnc';
 interface Props {
   type: 'minus' | 'plus';
   onPress: () => void;
+  active: boolean;
 }
 
-export default function CountBtn({ type, onPress }: Props) {
+export default function CountBtn({ type, onPress, active }: Props) {
+  const COLOR = type === 'minus' ? ORANGE_RED : DEEP_BLUE;
+  const COLOR_STRING = type === 'minus' ? 'orange' : 'blue';
+  const SIZE = `h-${scaleH(6.5)} w-${scaleH(6.5)}`;
+  const ACTIVE_STYLE = active ? `border border-${COLOR_STRING}-400` : '';
+
   return (
     <TouchableOpacity
-      style={tw`px-1 items-center justify-center border-2 rounded-full h-${scaleH(
-        7.5
-      )} w-${scaleH(7.5)} ${
-        type === 'minus' ? ' border-orange-200' : 'border-blue-200'
-      }`}
+      style={tw`items-center justify-center rounded-lg ${SIZE} ${ACTIVE_STYLE}`}
       onPress={onPress}
     >
       <Icon
         type='MaterialCommunityIcons'
         name={type === 'minus' ? 'minus' : 'plus'}
-        color={type === 'minus' ? ORANGE_RED : BLUE}
+        color={active ? COLOR : LIGHT_GRAY}
         size={18}
       />
     </TouchableOpacity>
--- components/screen-component/setting/CompartmentsSettingBox.tsx
+++ components/screen-component/setting/CompartmentsSettingBox.tsx
@@ -1,14 +1,15 @@
 import { Text } from '../../native-component';
 import { useDispatch, useSelector } from '../../../redux/hook';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import {
   minusCompartment,
   plusCompartment,
 } from '../../../redux/slice/fridgeInfoSlice';
 import { Space } from '../../../constant/fridgeInfo';
 import { scaleH } from '../../../util';
-import tw from 'twrnc';
 import CountBtn from '../../common/Buttons/CountBtn';
+import useGetFoodList from '../../../hooks/useGetFoodList';
+import tw from 'twrnc';
 
 interface Props {
   name: Space;
@@ -16,16 +17,28 @@ interface Props {
 
 export default function CompartmentsSettingBox({ name }: Props) {
   const { fridgeInfo } = useSelector((state) => state.fridgeInfo);
+  const { getFoodList } = useGetFoodList();
   const dispatch = useDispatch();
+
+  const MAX_COMPARTMENTS_NUM =
+    name === '냉동실 안쪽' || name === '냉동실 문쪽' ? 3 : 5;
 
   const onMinusPress = () => {
     if (fridgeInfo.compartments[name] <= 1) return;
+    const existLastCompartmentFood = !!getFoodList(name).filter(
+      (food) =>
+        +food.compartmentNum.slice(0, 1) === fridgeInfo.compartments[name]
+    ).length;
+    if (existLastCompartmentFood)
+      return Alert.alert(
+        '식료품 존재 안내',
+        `${fridgeInfo.compartments[name]}번 칸에 식료품이 있어 삭제할 수 없습니다.`
+      );
     dispatch(minusCompartment(name));
   };
 
   const onPlusPress = () => {
-    const maxNum = name === '냉동실 안쪽' || name === '냉동실 문쪽' ? 3 : 5;
-    if (fridgeInfo.compartments[name] >= maxNum) return;
+    if (fridgeInfo.compartments[name] >= MAX_COMPARTMENTS_NUM) return;
     dispatch(plusCompartment(name));
   };
 
@@ -36,12 +49,20 @@ export default function CompartmentsSettingBox({ name }: Props) {
       )}px] gap-3 justify-center flex-1 items-center bg-white border border-slate-300 rounded-md`}
     >
       <Text>{name}</Text>
-      <View style={tw`flex-row items-center justify-center`}>
-        <CountBtn type='minus' onPress={onMinusPress} />
-        <Text style={tw`mx-1 text-indigo-600 w-8 text-center`}>
+      <View style={tw`flex-row items-center justify-center gap-1`}>
+        <CountBtn
+          type='minus'
+          onPress={onMinusPress}
+          active={fridgeInfo.compartments[name] > 1}
+        />
+        <Text fontSize={15} style={tw`mx-1 text-indigo-600 text-center`}>
           {fridgeInfo.compartments[name]} 칸
         </Text>
-        <CountBtn type='plus' onPress={onPlusPress} />
+        <CountBtn
+          type='plus'
+          onPress={onPlusPress}
+          active={fridgeInfo.compartments[name] < MAX_COMPARTMENTS_NUM}
+        />
       </View>
     </View>
   );
