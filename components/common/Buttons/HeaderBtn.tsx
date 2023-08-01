ba0b6abf2b71b80b75d67a560feeaeeae8495ca5
jellie <talentforest0501@gmail.com>
2023-08-01 19:50:41 +0900

    delete setting screen

--- components/common/Buttons/HeaderBtn.tsx
+++ components/common/Buttons/HeaderBtn.tsx
@@ -1,4 +1,5 @@
 import { DEEP_GRAY } from '../../../constant/colors';
+import { scaleH } from '../../../util';
 import { TouchableOpacity } from '../../native-component';
 import Icon from '../../native-component/Icon';
 import tw from 'twrnc';
@@ -10,13 +11,11 @@ interface Props {
 
 export default function HeaderBtn({ iconName, onPress }: Props) {
   return (
-    <TouchableOpacity onPress={onPress} style={tw`px-4 py-1`}>
-      <Icon
-        name={iconName}
-        type={iconName === 'search' ? 'Ionicons' : 'MaterialCommunityIcons'}
-        size={22}
-        color={DEEP_GRAY}
-      />
+    <TouchableOpacity
+      onPress={onPress}
+      style={tw`px-[${scaleH(10)}px] py-[${scaleH(4)}px]`}
+    >
+      <Icon name={iconName} type='Ionicons' size={22} color={DEEP_GRAY} />
     </TouchableOpacity>
   );
 }
