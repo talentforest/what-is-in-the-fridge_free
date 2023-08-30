8633ba6e40b035490b4355c3c7be27a3522dc97a
jellie <talentforest0501@gmail.com>
2023-08-31 05:47:22 +0900

    organize buttons

--- components/common/Buttons/CountBtn.tsx
+++ components/common/Buttons/CountBtn.tsx
@@ -1,5 +1,4 @@
-import { DEEP_BLUE, LIGHT_GRAY, ORANGE_RED } from '../../../constant/colors';
-import { scaleH } from '../../../util';
+import { LIGHT_GRAY } from '../../../constant/colors';
 import { TouchableOpacity } from '../../native-component';
 import Icon from '../../native-component/Icon';
 import tw from 'twrnc';
@@ -11,22 +10,21 @@ interface Props {
 }
 
 export default function CountBtn({ type, onPress, active }: Props) {
-  const COLOR = type === 'minus' ? ORANGE_RED : DEEP_BLUE;
   const COLOR_STRING = type === 'minus' ? 'orange' : 'blue';
-  const SIZE = `h-${scaleH(6.5)} w-${scaleH(6.5)}`;
+  const SIZE = `h-6 w-6`;
   const ACTIVE_STYLE = active
-    ? `border border-${COLOR_STRING}-400 bg-${COLOR_STRING}-100`
-    : '';
+    ? `border border-${COLOR_STRING}-400 bg-${COLOR_STRING}-500`
+    : 'border border-slate-300 bg-slate-100';
 
   return (
     <TouchableOpacity
-      style={tw`items-center justify-center rounded-lg ${SIZE} ${ACTIVE_STYLE}`}
+      style={tw`items-center justify-center rounded-full ${SIZE} ${ACTIVE_STYLE}`}
       onPress={onPress}
     >
       <Icon
         type='MaterialCommunityIcons'
         name={type === 'minus' ? 'minus' : 'plus'}
-        color={active ? COLOR : LIGHT_GRAY}
+        color={active ? '#fff' : LIGHT_GRAY}
         size={18}
       />
     </TouchableOpacity>
--- components/common/Buttons/HeaderBtn.tsx
+++ components/common/Buttons/HeaderBtn.tsx
@@ -1,26 +1,38 @@
+import { useNavigation } from '@react-navigation/native';
 import { DEEP_GRAY } from '../../../constant/colors';
-import { scaleH } from '../../../util';
 import { TouchableOpacity } from '../../native-component';
+
 import Icon from '../../native-component/Icon';
 import tw from 'twrnc';
 
 interface Props {
-  iconName: string;
-  onPress: () => void;
   type?: 'MaterialCommunityIcons' | 'AntDesign' | 'Ionicons' | 'Feather';
+  iconName: string;
+  size: number;
+  onPress?: () => void;
+  backBtn?: boolean;
 }
 
 export default function HeaderBtn({
   iconName,
   onPress,
   type = 'Ionicons',
+  size,
+  backBtn,
 }: Props) {
+  const navigation = useNavigation();
+
+  const onBtnPress = () => {
+    if (backBtn) return navigation.goBack();
+    if (onPress) return onPress();
+  };
+
   return (
     <TouchableOpacity
-      onPress={onPress}
-      style={tw`px-[${scaleH(10)}px] py-[${scaleH(4)}px]`}
+      onPress={onBtnPress}
+      style={tw`px-${backBtn ? 2 : 4} py-2`}
     >
-      <Icon name={iconName} type={type} size={21} color={DEEP_GRAY} />
+      <Icon name={iconName} type={type} size={size} color={DEEP_GRAY} />
     </TouchableOpacity>
   );
 }
