e3dce372d622bd1d3f771afd8fd5f8dbc5d22387
jellie <talentforest0501@gmail.com>
2023-08-31 05:58:30 +0900

    organize utils

--- util/index.ts
+++ util/index.ts
@@ -1,7 +1,11 @@
-export * from './controlDate';
 export * from './cutLetter';
 export * from './deduplicate';
-export * from './getCompartments';
-export * from './getDate';
-export * from './scaleSize';
+
+// 냉장고 관련 함수
 export * from './findMatchNameFoods';
+export * from './getCompartments';
+export * from './filters';
+
+// 날짜 관련 함수
+export * from './controlDate';
+export * from './getDate';
