a05f8afe3870b9d11a644e6a50481d3a7b1b236e
jellie <talentforest0501@gmail.com>
2023-08-31 05:48:29 +0900

    UI fix: table

--- components/common/Table/TableHeader.tsx
+++ components/common/Table/TableHeader.tsx
@@ -1,45 +1,53 @@
+import { ReactNode } from 'react';
 import { View } from 'react-native';
 import { Text, TouchableOpacity } from '../../native-component';
-import { DEEP_INDIGO, INACTIVE_COLOR } from '../../../constant/colors';
-import { FavoriteFoodsFilter } from '../../../hooks/useTableItemFilter';
-import CheckBox from '../boxes/CheckBox';
-import tw from 'twrnc';
+import { BLUE, GRAY } from '../../../constant/colors';
 import { useSelector } from '../../../redux/hook';
+import { BoxColor } from '../../screen-component/home/EntranceBox';
+
+import CheckBox from '../CheckBox';
+import tw from 'twrnc';
 
 interface Props {
   title: string;
   entireChecked: boolean;
   onEntirePress: () => void;
-  columnTitle: '추가' | '유통기한순' | '냉장고 유무';
+  children?: ReactNode;
+  color: BoxColor;
 }
 
 export default function TableHeader({
   title,
   entireChecked,
   onEntirePress,
-  columnTitle,
+  children,
+  color,
 }: Props) {
   const { shoppingList } = useSelector((state) => state.shoppingList);
 
   return (
-    <View style={tw`gap-1 flex-row items-center justify-between`}>
-      <TouchableOpacity onPress={onEntirePress}>
-        <CheckBox
-          checked={entireChecked}
-          activeColor={entireChecked ? DEEP_INDIGO : INACTIVE_COLOR}
-        />
-      </TouchableOpacity>
-      <Text style={tw`text-slate-800`}>{title}</Text>
-      {title === '장봐야할 식료품' && (
-        <Text style={tw`text-slate-500`}>{shoppingList.length}개</Text>
-      )}
-      <View
-        style={tw`flex-1 justify-end flex-row items-center gap-0.5 rounded-full`}
-      >
-        <Text style={tw`text-slate-500`} fontSize={13}>
-          {columnTitle}
-        </Text>
+    <View
+      style={tw`px-3 py-2 h-10 gap-3 flex-row items-center justify-between`}
+    >
+      <View style={tw`gap-1 flex-row items-center flex-1`}>
+        <TouchableOpacity onPress={onEntirePress}>
+          <CheckBox
+            checked={entireChecked}
+            activeColor={entireChecked ? BLUE : GRAY}
+          />
+        </TouchableOpacity>
+
+        <View style={tw`flex-1 flex-row gap-1.5 items-center`}>
+          <Text style={tw`text-${color}-700 text-sm`}>{title}</Text>
+          {title === '장봐야할 식료품' && (
+            <Text style={tw`text-${color}-700 flex-1`}>
+              {shoppingList.length}개
+            </Text>
+          )}
+        </View>
       </View>
+
+      {children}
     </View>
   );
 }
