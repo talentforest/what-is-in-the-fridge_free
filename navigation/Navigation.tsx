bcf72e25c87330c8b9e9cee36e48a627a0926f44
jellie <talentforest0501@gmail.com>
2023-08-31 06:01:57 +0900

    UI fix: navigation

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -4,17 +4,18 @@ import {
 } from '@react-navigation/native-stack';
 import { NavigationProp } from '@react-navigation/native';
 import { DEEP_GRAY, HEADER_BGCOLOR } from '../constant/colors';
+import { FontGmarketSansRegular } from '../constant/fonts';
+
 import MyTabs, { RootTabParamList } from './MyTabs';
-import React from 'react';
 import Compartments from '../screens/Compartments';
 import FavoriteFoods from '../screens/FavoriteFoods';
 import ExpiredFoods from '../screens/ExpiredFoods';
-import BackBtn from '../components/common/buttons/BackBtn';
 import FridgeSetting from '../screens/FridgeSetting';
-import HeaderTitle from '../components/common/HeaderTitle';
+import HeaderBtn from '../components/common/buttons/HeaderBtn';
 
 export type RootStackParamList = {
   MyTabs: undefined;
+  OnBoarding: undefined;
   Compartments: undefined | object;
   FavoriteFoods: undefined;
   ExpiredFoods: undefined;
@@ -31,10 +32,23 @@ const options: NativeStackNavigationOptions = {
   headerShown: true,
   headerTintColor: DEEP_GRAY,
   headerShadowVisible: false,
-  headerStyle: { backgroundColor: HEADER_BGCOLOR },
+  headerStyle: {
+    backgroundColor: HEADER_BGCOLOR,
+  },
   headerTitleAlign: 'center',
-  headerLeft: () => <BackBtn />,
+  headerLeft: () => (
+    <HeaderBtn
+      backBtn
+      type='MaterialCommunityIcons'
+      iconName='chevron-left'
+      size={26}
+    />
+  ),
   headerBackVisible: false, // android
+  headerTitleStyle: {
+    fontSize: 18,
+    ...FontGmarketSansRegular,
+  },
 };
 
 const Navigation = () => {
@@ -55,7 +69,7 @@ const Navigation = () => {
         component={FavoriteFoods}
         options={{
           ...options,
-          headerTitle: () => <HeaderTitle title='자주 먹는 식료품 관리' />,
+          title: '자주 먹는 식료품 관리',
         }}
       />
       <Stack.Screen
@@ -63,7 +77,7 @@ const Navigation = () => {
         component={ExpiredFoods}
         options={{
           ...options,
-          headerTitle: () => <HeaderTitle title='유통기한 주의 식료품 관리' />,
+          title: '유통기한 주의 식료품 관리',
         }}
       />
       <Stack.Screen
@@ -71,7 +85,7 @@ const Navigation = () => {
         component={FridgeSetting}
         options={{
           ...options,
-          headerTitle: () => <HeaderTitle title='나의 냉장고 설정' />,
+          title: '나의 냉장고 설정',
         }}
       />
     </Stack.Navigator>
