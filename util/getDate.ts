dc072f71fa822d601cec24706e387d247620d6ea
jellie <talentforest0501@gmail.com>
2023-08-31 05:58:30 +0900

    organize utils

--- util/getDate.ts
+++ util/getDate.ts
@@ -6,10 +6,14 @@ dayjs.extend(relativeTime);
 dayjs.locale('ko');
 
 export const getRelativeTime = (expiryDate: string) => {
+  const yesterday = dayjs().subtract(1, 'day').format('YYYY-MM-DD');
   const today = dayjs().format('YYYY-MM-DD');
+  const tomorrow = dayjs().add(1, 'day').format('YYYY-MM-DD');
   const date = dayjs(expiryDate).format('YYYY-MM-DD');
 
+  if (yesterday === date) return '어제';
   if (today === date) return '오늘';
+  if (tomorrow === date) return '내일';
   return dayjs(expiryDate).fromNow();
 };
 
