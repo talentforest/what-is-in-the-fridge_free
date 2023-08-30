72f27cb4b23abc4770044fb12a011ff41b1a1767
jellie <talentforest0501@gmail.com>
2023-08-31 05:49:59 +0900

    refactoring custom native component

--- components/native-component/SafeBottomAreaView.tsx
+++ components/native-component/SafeBottomAreaView.tsx
@@ -1,8 +1,13 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { BG_COLOR } from '../common/Container';
 import tw from 'twrnc';
 
 export function SafeBottomAreaView({ ...props }) {
   return (
-    <SafeAreaView edges={['bottom']} style={tw`flex-1 bg-blue-50`} {...props} />
+    <SafeAreaView
+      edges={['bottom']}
+      style={tw`flex-1 ${BG_COLOR}`}
+      {...props}
+    />
   );
 }
