549be97e12d0738b482b2271a40d6fab1c6bcbd3
jellie <talentforest0501@gmail.com>
2023-09-11 21:11:53 +0900

    feat: 펜트리 option in space item of form

--- constant/formInfo.ts
+++ constant/formInfo.ts
@@ -3,13 +3,12 @@ export type FormStepName = '식품 정보' | '식품 위치' | '식품 날짜';
 export type FormStep = { step: number; name: FormStepName };
 
 export type FormLabelType =
-  | '냉장고 위치 선택'
+  | '추가할 식료품의 위치'
   | '식료품 이름'
   | '카테고리'
   | '구매날짜'
   | '유통기한'
-  | '자주 먹는 식료품'
-  | '추가사항';
+  | '자주 먹는 식료품';
 
 export const foodForm = [
   '식료품 이름',
