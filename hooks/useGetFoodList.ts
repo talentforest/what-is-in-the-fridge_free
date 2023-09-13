8496b7656b21066cdf1e01f67a41cbc4633e63f8
jellie <talentforest0501@gmail.com>
2023-09-14 04:36:51 +0900

    bug fix: filter favorite pantry foods

--- hooks/useGetFoodList.ts
+++ hooks/useGetFoodList.ts
@@ -84,7 +84,7 @@ export const useGetFoodList = () => {
 
     if (filter === '자주 먹는 식료품') {
       const list = foodList.filter((food) =>
-        favoriteFoods.find((favFood) => food.name !== favFood.name)
+        favoriteFoods.find((favFood) => food.name === favFood.name)
       );
       return orderExpirationDate(list);
     }
