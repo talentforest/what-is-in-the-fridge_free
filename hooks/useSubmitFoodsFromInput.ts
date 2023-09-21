d7b6b6acb9a351818d8de2c8c766be86122eab67
jellie <talentforest0501@gmail.com>
2023-09-21 17:11:23 +0900

    refactoring: submit from input hook

---
+++ hooks/useSubmitFoodsFromInput.ts
@@ -0,0 +1,92 @@
+import { Alert, Keyboard } from 'react-native';
+import { useDispatch, useSelector } from '../redux/hook';
+import { addFavorite } from '../redux/slice/favoriteFoodsSlice';
+import { Food, initialFood } from '../constant/foodInfo';
+import { Category } from '../constant/foodCategories';
+import { addToShoppingList } from '../redux/slice/shoppingListSlice';
+import { AnimationState } from './animation/useSetAnimationState';
+import UUIDGenerator from 'react-native-uuid';
+
+export const useSubmitFoodsFromInput = () => {
+  const { pantryFoods } = useSelector((state) => state.pantryFoods);
+  const { fridgeFoods } = useSelector((state) => state.fridgeFoods);
+  const { favoriteFoods } = useSelector((state) => state.favoriteFoods);
+
+  const dispatch = useDispatch();
+  const myUuid = UUIDGenerator.v4();
+
+  const findFood = (name: string) => {
+    return [...pantryFoods, ...fridgeFoods].find((food) => food.name === name);
+  };
+
+  const onSubmitFavoriteListItem = (
+    inputValue: string,
+    category: Category | '',
+    setInputValue: (value: string) => void,
+    setCategory: (category: Category | '') => void,
+    setShowCaution: (caution: boolean) => void
+  ) => {
+    if (inputValue === '') return Keyboard.dismiss();
+
+    const existFavoriteFood = favoriteFoods.find(
+      (food) => food.name === inputValue
+    );
+    if (existFavoriteFood || category === '') return setShowCaution(true);
+
+    if (findFood(inputValue)) {
+      Alert.alert(
+        '알림',
+        `동일한 이름의 식료품이 ${
+          findFood(inputValue)?.space
+        }에 존재합니다. 해당 정보로 저장됩니다`,
+        [
+          {
+            text: '저장',
+            onPress: () => {
+              dispatch(addFavorite(findFood(inputValue) as Food));
+              setCategory('');
+              setInputValue('');
+            },
+            style: 'default',
+          },
+        ]
+      );
+    } else {
+      dispatch(
+        addFavorite({
+          ...initialFood,
+          id: myUuid as string,
+          name: inputValue,
+          category,
+        })
+      );
+    }
+  };
+
+  const onSubmitShoppingListItem = (
+    name: string,
+    setAnimationState: (state: AnimationState) => void
+  ) => {
+    const { expiredDate, purchaseDate } = initialFood;
+
+    const food = Object.keys(findFood(name) || {}).length
+      ? ({
+          ...findFood(name),
+          expiredDate,
+          purchaseDate,
+        } as Food)
+      : {
+          ...initialFood,
+          id: myUuid as string,
+          name,
+        };
+
+    dispatch(addToShoppingList(food));
+    setAnimationState('slidedown-in');
+  };
+
+  return {
+    onSubmitFavoriteListItem,
+    onSubmitShoppingListItem,
+  };
+};
