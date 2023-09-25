0946a29aa1ee7d1e73ad6c40a138059d708ad1de
jellie <talentforest0501@gmail.com>
2023-09-25 20:41:14 +0900

    bug fix: diff date

--- util/getDate.ts
+++ util/getDate.ts
@@ -6,8 +6,9 @@ dayjs.extend(relativeTime);
 dayjs.locale('ko');
 
 export const getDiffDate = (date: string) => {
-  const diff = dayjs(date).diff(dayjs(), 'day', true);
-  return +diff.toFixed(0) + 1;
+  const today = dayjs().format('YYYY-MM-DD');
+  const diffDate = dayjs(date).diff(today, 'day');
+  return diffDate;
 };
 
 export const getRelativeTime = (date: string | Date) => {
