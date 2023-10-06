b43713f9f55017b40c3a216f1854cdf736977189
jellie <talentforest0501@gmail.com>
2023-10-07 04:03:24 +0900

    delete more open button at home

--- components/common/FoodTagBox.tsx
+++ components/common/FoodTagBox.tsx
@@ -13,12 +13,12 @@ interface Props {
 export default function FoodTagBox({ food }: Props) {
   const navigation = useNavigation<NavigateProp>();
 
-  return (
+  return food.name ? (
     <TouchableOpacity
       onPress={() => navigation.navigate('ShoppingList')}
       key={food.id}
       style={tw.style(
-        `py-1 px-3 bg-white border border-slate-300 gap-1 justify-center items-center flex-row rounded-lg`,
+        `py-1 px-3 h-9 min-w-13 bg-white border border-slate-300 gap-1 justify-center items-center flex-row rounded-lg`,
         shadowStyle(5)
       )}
     >
@@ -26,5 +26,7 @@ export default function FoodTagBox({ food }: Props) {
         {cutLetter(food.name, 14)}
       </Text>
     </TouchableOpacity>
+  ) : (
+    <></>
   );
 }
