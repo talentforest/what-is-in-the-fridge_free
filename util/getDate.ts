01c5a8aaaf8e3315c141eab45ca81ae803777f9b
jellie <talentforest0501@gmail.com>
2023-10-07 16:08:23 +0900

    bug fix: dayjs range

--- util/getDate.ts
+++ util/getDate.ts
@@ -1,9 +1,28 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 
-dayjs.extend(relativeTime);
+const config = {
+  thresholds: [
+    { l: 's', r: 1 },
+    { l: 'm', r: 1 },
+    { l: 'mm', r: 59, d: 'minute' },
+    { l: 'h', r: 1 },
+    { l: 'hh', r: 23, d: 'hour' },
+    { l: 'd', r: 1 },
+    { l: 'dd', r: 29, d: 'day' },
+    { l: 'M', r: 1 },
+    { l: 'MM', r: 11, d: 'month' },
+    { l: 'y', r: 1 },
+    { l: 'yy', d: 'year' },
+  ],
+  rounding: Math.floor,
+};
+
+dayjs.extend(relativeTime, config);
 dayjs.locale('ko');
+dayjs.extend(updateLocale);
 
 export const getDiffDate = (date: string) => {
   const today = dayjs().format('YYYY-MM-DD');
@@ -31,6 +50,4 @@ export const getRelativeTime = (date: string | Date) => {
 export const getFormattedDate = (
   date: string | Date,
   format: 'YYYY-MM-DD' | 'YYYY년 MM월 DD일' | 'YYYY.MM.DD'
-) => {
-  return dayjs(date).format(format);
-};
+) => dayjs(date).format(format);
